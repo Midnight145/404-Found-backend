c5dab7cc1f5733fb60cc8ca924a0d368c8078370
Midnight <23142346+Midnight145@users.noreply.github.com>
2025-11-04 11:20:05 -0600

    habit_delete auth

--- modules/habits.py
+++ modules/habits.py
@@ -10,7 +10,22 @@ router = fastapi.APIRouter()
 def habit_create(info: HabitInfo, response: fastapi.Response):
     with Database() as db:
         print(*SQLHelper.habit_create(info))
-        db.execute(*SQLHelper.habit_create(info))
-        response.status_code = 200
-        db.write()
+        if db.execute(*SQLHelper.habit_create(info)):
+            response.status_code = 200
+            db.write()
+        else:
+            response.status_code = 500
+            return response
+        habit_id = db.created_id()
+    return {"habit_id": habit_id}
 
+@router.post("/habit/delete")
+def habit_delete(habit_id: int, response: fastapi.Response):
+    with Database() as db:
+        if db.execute(*SQLHelper.habit_delete(habit_id)):
+            response.status_code = 200
+            db.write()
+        else:
+            response.status_code = 500
+            return response
+    return {"success": True}
--- state/SQLHelper.py
+++ state/SQLHelper.py
@@ -6,3 +6,7 @@ def habit_create(info: HabitInfo):
     query = "INSERT INTO habits (account_id, habit_name, habit_desc, steps, repeat, repeat_type, reward, reward_frequency) VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
     steps_str = json.dumps(info.steps)
     return query, (info.account_id, info.habit_name, info.habit_desc, steps_str, 1 if info.repeat else 0, info.repeat_type, info.reward, info.reward_frequency)
+
+def habit_delete(habit_id: int):
+    query = "DELETE FROM habits WHERE habit_id = ?"
+    return query, (habit_id,)
